6b6b13a1a93de774b34560c7972cb91a495be147
liujian1 <liujian1@gomeholdings.com>
2016-10-18 15:40:03 +0800

    删除warnings

--- aposoft-wechat-core/src/test/java/cn/aposoft/wechat/mp/config/basic/BasicWechatMpConfigTest.java
+++ aposoft-wechat-core/src/test/java/cn/aposoft/wechat/mp/config/basic/BasicWechatMpConfigTest.java
@@ -6,31 +6,30 @@ import static org.mockito.Mockito.when;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class BasicWechatMpConfigTest {
 
-	BasicWechatMpConfig config;
+    BasicWechatMpConfig config;
 
-	@Before
-	public void init() {
-		config = mock(BasicWechatMpConfig.class);
-		String userId = "gh_0f504b63df22";
-		String appId = "wx31659662068251dc";
-		String appSecret = "9cf9858af4718fde40d67968b5de3967";
-		String token = "AposoftBugs";
-		int expired_threshold = 300;
-		when(config.getAppId()).thenReturn(appId).thenThrow(new NullPointerException());
-		// when(config.getAppId()).thenThrow(new RuntimeException());
-		when(config.getUserId()).thenReturn(userId);
-		when(config.getAppSecret()).thenReturn(appSecret);
-		when(config.getToken()).thenReturn(token);
-		when(config.getExpiredThreshold()).thenReturn(expired_threshold);
-	}
+    @Before
+    public void init() {
+        config = mock(BasicWechatMpConfig.class);
+        String userId = "gh_0f504b63df22";
+        String appId = "wx31659662068251dc";
+        String appSecret = "9cf9858af4718fde40d67968b5de3967";
+        String token = "AposoftBugs";
+        int expired_threshold = 300;
+        when(config.getAppId()).thenReturn(appId).thenThrow(new NullPointerException());
+        // when(config.getAppId()).thenThrow(new RuntimeException());
+        when(config.getUserId()).thenReturn(userId);
+        when(config.getAppSecret()).thenReturn(appSecret);
+        when(config.getToken()).thenReturn(token);
+        when(config.getExpiredThreshold()).thenReturn(expired_threshold);
+    }
 
-	@Test
-	public void testGetAppId() {
-		Assert.assertEquals("wx31659662068251dc", config.getAppId());
-	}
+    @Test
+    public void testGetAppId() {
+        Assert.assertEquals("wx31659662068251dc", config.getAppId());
+    }
 
 }
