0f3d90a9a656ce70a1c4ee67526f04d4ddfd599e
pipi668 <pleasantboy@qq.com>
2017-05-19 14:46:04 +0800

    调整Charset定义方式，和UTF-8Name定义

--- aposoft-wechat-core/src/main/java/cn/aposoft/constant/Lexical.java
+++ aposoft-wechat-core/src/main/java/cn/aposoft/constant/Lexical.java
@@ -4,25 +4,25 @@
 package cn.aposoft.constant;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 通用词汇常量
  * 
  * @author LiuJian
  * @date 2016年10月13日
- * 
  */
 public class Lexical {
-    /**
-     * UTF-8
-     */
-    public static final String UTF8 = "UTF-8";
-    /**
-     * UTF-8 Character encoding
-     */
-    public static final Charset UTF8_CHARSET = Charset.forName(UTF8);
-    /**
-     * JSON : "json"
-     */
-    public static final String JSON = "json";
+	/**
+	 * UTF-8 Character encoding
+	 */
+	public static final Charset UTF8_CHARSET = StandardCharsets.UTF_8;
+	/**
+	 * UTF-8
+	 */
+	public static final String UTF8 = UTF8_CHARSET.displayName();
+	/**
+	 * JSON : "json"
+	 */
+	public static final String JSON = "json";
 }
