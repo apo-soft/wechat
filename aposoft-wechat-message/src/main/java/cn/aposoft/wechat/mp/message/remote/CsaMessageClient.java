b7a179aa5f527270090dbafd53ed7929faa02608
pipi668 <pleasantboy@qq.com>
2017-05-22 15:14:15 +0800

    优化实现

--- aposoft-wechat-message/src/main/java/cn/aposoft/wechat/mp/message/remote/CsaMessageClient.java
+++ aposoft-wechat-message/src/main/java/cn/aposoft/wechat/mp/message/remote/CsaMessageClient.java
@@ -5,14 +5,8 @@ package cn.aposoft.wechat.mp.message.remote;
 
 import java.io.Closeable;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.client.entity.EntityBuilder;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.HttpClientUtils;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
-
-import com.alibaba.fastjson.JSON;
 
 import cn.aposoft.util.HttpClient;
 import cn.aposoft.util.HttpClientFactory;
@@ -40,18 +34,7 @@ public class CsaMessageClient implements Closeable {
 		if (StringUtil.isBlank(accessToken, msg)) {
 			throw new IllegalArgumentException("Some argument(s) is null or empty.");
 		}
-		final String requestUrl = getCsaMessageUrl(accessToken);
-		HttpPost httpPost = new HttpPost(requestUrl);
-		httpPost.setEntity(EntityBuilder.create()//
-				.setContentType(ContentType.APPLICATION_JSON)//
-				.setText(JSON.toJSONString(msg))//
-				.build());
-		String respMsg = HttpClient.execute(httpPost, httpClient);
-
-		if (StringUtils.isBlank(respMsg)) {
-			throw new RemoteException("Empty response message.");
-		}
-		return JSON.parseObject(respMsg, WechatResp.class);
+		return HttpClient.executeWechat(HttpClient.createJsonHttpPost(getCsaMessageUrl(accessToken), msg), httpClient);
 	}
 
 	private String getCsaMessageUrl(String accessToken) {
