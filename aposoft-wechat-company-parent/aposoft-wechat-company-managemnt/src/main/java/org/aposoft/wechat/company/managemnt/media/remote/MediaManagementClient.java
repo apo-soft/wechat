63cfe6401a9debe55209c7e7536195e4d6178658
Bugs <pleasantboy@qq.com>
2017-05-30 23:51:11 +0800

    添加企业临时素材管理功能

---
+++ aposoft-wechat-company-parent/aposoft-wechat-company-managemnt/src/main/java/org/aposoft/wechat/company/managemnt/media/remote/MediaManagementClient.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package org.aposoft.wechat.company.managemnt.media.remote;
+
+import java.io.Closeable;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import com.alibaba.fastjson.JSON;
+
+import cn.aposoft.util.AposoftHttpEntity;
+import cn.aposoft.util.HttpClient;
+import cn.aposoft.util.HttpClientFactory;
+import cn.aposoft.util.MediaEntity;
+import cn.aposoft.util.RemoteException;
+import cn.aposoft.util.StringUtil;
+import cn.aposoft.wechat.mp.media.remote.MediaEntityResp;
+import cn.aposoft.wechat.mp.media.remote.MediaResp;
+
+/**
+ * 临时素材管理服务
+ * 
+ * @author Jann Liu
+ *
+ */
+public class MediaManagementClient implements Closeable {
+	static final CloseableHttpClient httpClient = HttpClientFactory.createDefault();
+
+	//
+	static final String COMPANY_MEDIA_UPLOAD_URL = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=";
+	static final String COMPANY_MEDIA_UPLOAD_TYPE_PARAM_URL = "&type=";
+
+	static final String COMPANY_MEDIA_GET_URL = "https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=";
+	static final String COMPANY_GET_MEIDA_ID_PARAM_URL = "&media_id=";
+
+	@Override
+	public void close() {
+		HttpClientUtils.closeQuietly(httpClient);
+	}
+
+	public MediaResp upload(final String accessToken, final String type, MediaEntity media) throws RemoteException {
+		if (media == null || media.getEntity() == null || media.getEntity().length == 0) {
+			throw new IllegalArgumentException("media is null or empty.");
+		}
+
+		if (StringUtil.isBlank(accessToken, media, media.getFilename(), media.getContentType())) {
+			throw new IllegalArgumentException("Some argument(s) is null or empty.");
+		}
+		final String requestUrl = getUploadMediaUrl(accessToken, type);
+		HttpPost httpPost = new HttpPost(requestUrl);
+		httpPost.setEntity(MultipartEntityBuilder.create().addBinaryBody("media", media.getEntity(),
+				ContentType.create(media.getContentType()), media.getFilename()).build());
+		return HttpClient.execute(httpPost, MediaResp.class, httpClient);
+	}
+
+	private String getUploadMediaUrl(String accessToken, String type) {
+		return COMPANY_MEDIA_UPLOAD_URL + accessToken + COMPANY_MEDIA_UPLOAD_TYPE_PARAM_URL + type;
+	}
+
+	/**
+	 * 
+	 * @param accessToken
+	 *            访问授权码
+	 * @param meidaId
+	 *            素材ID
+	 * @return 素材信息
+	 * @throws RemoteException
+	 */
+	public MediaEntityResp getMeida(final String accessToken, final String mediaId) throws RemoteException {
+		if (StringUtil.isBlank(accessToken, mediaId)) {
+			throw new IllegalArgumentException("Some argument(s) is null or empty.");
+		}
+		final String requestUrl = getMediaUrl(accessToken, mediaId);
+		HttpGet httpPost = new HttpGet(requestUrl);
+
+		AposoftHttpEntity entity = HttpClient.executeEntity(httpPost, httpClient);
+		if (ContentType.APPLICATION_JSON.getMimeType().equals(entity.getMimeType())) {
+			return JSON.parseObject(entity.getText(), MediaEntityResp.class);
+		} else {
+			MediaEntityResp resp = new MediaEntityResp();
+			resp.setMedia(entity.getMediaEntity());
+			return resp;
+		}
+	}
+
+	private String getMediaUrl(String accessToken, String mediaId) {
+		return COMPANY_MEDIA_GET_URL + accessToken + COMPANY_GET_MEIDA_ID_PARAM_URL + mediaId;
+	}
+
+}
