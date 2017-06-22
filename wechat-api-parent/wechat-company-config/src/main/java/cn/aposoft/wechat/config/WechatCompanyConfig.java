0d9ee095513291b4e9395491f65c400fd2566157
Bugs <pleasantboy@qq.com>
2017-06-23 00:23:17 +0800

    持久化存储接口添加

--- wechat-api-parent/wechat-company-config/src/main/java/cn/aposoft/wechat/config/WechatCompanyConfig.java
+++ wechat-api-parent/wechat-company-config/src/main/java/cn/aposoft/wechat/config/WechatCompanyConfig.java
@@ -5,7 +5,6 @@ package cn.aposoft.wechat.config;
 
 import java.io.Serializable;
 
-import cn.aposoft.wechat.AccountType;
 import cn.aposoft.wechat.AccountTypeAware;
 import cn.aposoft.wechat.signature.SignatureConfig;
 
@@ -15,70 +14,6 @@ import cn.aposoft.wechat.signature.SignatureConfig;
  * @author Jann Liu
  * @since 1.0
  */
-public class WechatCompanyConfig implements AccountTypeAware, SignatureConfig, CompanyAccountConfig, Serializable {
-	private static final long serialVersionUID = 6960974749855213481L;
-	private String id;
-	private String secret;
-	private Integer agentId;
-
-	@Override
-	public AccountType getAccountType() {
-		return AccountType.CORP;
-	}
-
-	/**
-	 * @return the corpId
-	 */
-	public String getId() {
-		return id;
-	}
-
-	/**
-	 * @param corpId
-	 *            the corpId to set
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the Secret
-	 */
-	public String getSecret() {
-		return secret;
-	}
-
-	/**
-	 * @param secret
-	 *            the secret to set
-	 */
-	public void setSecret(String secret) {
-		this.secret = secret;
-	}
-
-	/**
-	 * @return the agentId
-	 */
-	public Integer getAgentId() {
-		return agentId;
-	}
-
-	/**
-	 * @param agentId
-	 *            the agentId to set
-	 */
-	public void setAgentId(Integer agentId) {
-		this.agentId = agentId;
-	}
-
-	@Override
-	public String getToken() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String getEncodingAESKey() {
-		throw new UnsupportedOperationException();
-	}
+public interface WechatCompanyConfig extends AccountTypeAware, SignatureConfig, CompanyAccountConfig, Serializable {
 
 }
---
+++ wechat-api-parent/wechat-company-config/src/main/java/cn/aposoft/wechat/config/WechatRepoConfig.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package cn.aposoft.wechat.config;
+
+/**
+ * Wechat的配置持久化存储格式
+ * 
+ * @author Jann Liu
+ * @since 1.0
+ */
+public interface WechatRepoConfig extends WechatMpConfig, WechatCompanyConfig {
+
+}
