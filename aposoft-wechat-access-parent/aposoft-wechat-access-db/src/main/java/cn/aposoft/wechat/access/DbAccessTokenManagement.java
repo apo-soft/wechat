a86dcc9948014060ccfa938f128532bb254fcf22
Bugs <pleasantboy@qq.com>
2017-06-27 21:20:44 +0800

    添加数据库访问方法

--- aposoft-wechat-access-parent/aposoft-wechat-access-db/src/main/java/cn/aposoft/wechat/access/DbAccessTokenManagement.java
+++ aposoft-wechat-access-parent/aposoft-wechat-access-db/src/main/java/cn/aposoft/wechat/access/DbAccessTokenManagement.java
@@ -3,12 +3,14 @@
  */
 package cn.aposoft.wechat.access;
 
+import java.util.List;
+
 import cn.aposoft.wechat.AccountId;
+import cn.aposoft.wechat.AccountType;
 import cn.aposoft.wechat.CompanyAccountId;
-import cn.aposoft.wechat.access.AccessToken;
-import cn.aposoft.wechat.access.CompanyAccessTokenManagement;
-import cn.aposoft.wechat.access.CompanyAccessTokenStore;
-import cn.aposoft.wechat.access.MpAccessTokenStore;
+import cn.aposoft.wechat.access.repo.DbAccessToken;
+import cn.aposoft.wechat.access.repo.DbAccessTokenExample;
+import cn.aposoft.wechat.access.repo.DbAccessTokenMapper;
 
 /**
  * 使用Database完成accessToken的缓存
@@ -18,31 +20,89 @@ import cn.aposoft.wechat.access.MpAccessTokenStore;
  */
 public class DbAccessTokenManagement implements CompanyAccessTokenManagement {
 
-	@Override
-	public void close() {
+	private DbAccessTokenMapper dbAccessTokenMapper;
 
+	private static AccessToken wrap(AccessToken accessToken) {
+		return new AccessTokenWrapper(accessToken);
+	}
+
+	public DbAccessTokenManagement() {
+	}
+
+	public DbAccessTokenManagement(DbAccessTokenMapper dbAccessTokenMapper) {
+		this.dbAccessTokenMapper = dbAccessTokenMapper;
 	}
 
 	@Override
 	public void setAccessToken(MpAccessTokenStore token) {
-
+		
 	}
 
+	/**
+	 * 综合处理一般性MpAccountId 和CompanyAccountId
+	 */
 	@Override
 	public AccessToken getAccessToken(AccountId accountId) {
-		return null;
+		if (accountId == null) {
+			return null;
+		} else if (accountId instanceof CompanyAccountId) {
+			return getAccessToken((CompanyAccountId) accountId);
+		} else {
+			DbAccessTokenExample example = new DbAccessTokenExample();
+			example.createCriteria().andAccountTypeEqualTo(AccountType.MP.name()).andIdEqualTo(accountId.getId());
+			List<DbAccessToken> accessTokens = dbAccessTokenMapper.selectByExample(example);
+			if (accessTokens != null && !accessTokens.isEmpty()) {
+				return wrap(accessTokens.get(0));
+			} else {
+				return null;
+			}
+		}
 	}
 
+	/**
+	 * 读取 AccessToken
+	 */
 	@Override
 	public AccessToken getAccessToken(CompanyAccountId accountId) {
-		// TODO Auto-generated method stub
-		return null;
+		if (accountId == null) {
+			return null;
+		} else {
+			DbAccessTokenExample example = new DbAccessTokenExample();
+			example.createCriteria().andAccountTypeEqualTo(AccountType.CORP.name()).andIdEqualTo(accountId.getId())
+					.andAgentIdEqualTo(accountId.getAgentId());
+			List<DbAccessToken> accessTokens = dbAccessTokenMapper.selectByExample(example);
+			if (accessTokens != null && !accessTokens.isEmpty()) {
+				return wrap(accessTokens.get(0));
+			} else {
+				return null;
+			}
+		}
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void setAccessToken(CompanyAccessTokenStore token) {
-		// TODO Auto-generated method stub
+		
+	}
 
+	/**
+	 * @param dbAccessTokenMapper
+	 *            the dbAccessTokenMapper to set
+	 */
+	public void setDbAccessTokenMapper(DbAccessTokenMapper dbAccessTokenMapper) {
+		this.dbAccessTokenMapper = dbAccessTokenMapper;
+	}
+
+	static class AccessTokenWrapper extends BasicAccessToken implements AccessToken {
+		private static final long serialVersionUID = 1381546217931851968L;
+
+		AccessTokenWrapper(AccessToken accessToken) {
+			this.setAccess_token(accessToken.getAccess_token());
+			this.setExpires_in(accessToken.getExpires_in());
+			this.setRefreshTime(accessToken.getRefreshTime());
+		}
 	}
 
 }
--- wechat-api-parent/wechat-access/src/main/java/cn/aposoft/wechat/access/AccessTokenManagement.java
+++ wechat-api-parent/wechat-access/src/main/java/cn/aposoft/wechat/access/AccessTokenManagement.java
@@ -1,14 +1,12 @@
 package cn.aposoft.wechat.access;
 
-import cn.aposoft.io.QuietCloseable;
-
 /**
  * 设置及更新公众号和企业号的AccessToken的本地存储
  * 
  * @author Jann Liu
  * @since 1.0
  */
-public interface AccessTokenManagement extends AccessTokenAccess, QuietCloseable {
+public interface AccessTokenManagement extends AccessTokenAccess {
 
 	/**
 	 * 
