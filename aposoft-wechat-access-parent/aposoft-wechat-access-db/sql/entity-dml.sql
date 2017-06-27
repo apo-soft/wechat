d27616bec32d96e176d6bd074341d122b5742a04
Bugs <pleasantboy@qq.com>
2017-06-27 14:57:48 +0800

    变更db.sql等文件结构

--- aposoft-wechat-access-parent/aposoft-wechat-access-db/sql/entity-dml.sql
+++ aposoft-wechat-access-parent/aposoft-wechat-access-db/sql/entity-dml.sql
@@ -1,4 +1,4 @@
-create user 'wechat_test'@'localhost' identified by 'test_pass';
+create user 'wechat_test'@'localhost' identified by 'wechat_test';
 grant all privileges on wechat_dev.* to 'wechat_test'@'localhost';
 
 CREATE TABLE `access_token` (
@@ -10,15 +10,3 @@ CREATE TABLE `access_token` (
   `refresh_time` datetime DEFAULT NULL COMMENT '刷新时间',
   KEY `access_token_account_index` (`id`,`account_type`,`agent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ACCESS_TOKEN缓存表';
-
-
-CREATE TABLE `access_config` (
-  `account_type` varchar(10) NOT NULL COMMENT '账户类型',
-  `id` varchar(45) NOT NULL COMMENT '公众号,服务号,企业号id',
-  `agent_id` int(11) DEFAULT NULL COMMENT '企业应用id,公众号,服务号无此项',
-  `access_token_secret` varchar(45) NOT NULL COMMENT '密钥用于获取access_token',
-  `aes_key` varchar(45) DEFAULT NULL COMMENT 'aes加密key',
-  `callback_token` varchar(45) DEFAULT NULL COMMENT '回调Token',
-  `createTime` datetime DEFAULT CURRENT_TIMESTAMP,
-  UNIQUE KEY `access_config` (`id`,`account_type`,`agent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='公众号,服务号,企业号访问配置';
---
+++ aposoft-wechat-access-parent/aposoft-wechat-access-db/src/main/java/cn/aposoft/wechat/access/db/DbAccessTokenManagement.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package cn.aposoft.wechat.access.db;
+
+import cn.aposoft.wechat.AccountId;
+import cn.aposoft.wechat.CompanyAccountId;
+import cn.aposoft.wechat.access.AccessToken;
+import cn.aposoft.wechat.access.CompanyAccessTokenManagement;
+import cn.aposoft.wechat.access.CompanyAccessTokenStore;
+import cn.aposoft.wechat.access.MpAccessTokenStore;
+
+/**
+ * 使用Database完成accessToken的缓存
+ * 
+ * @author Jann Liu
+ * @since 1.0
+ */
+public class DbAccessTokenManagement implements CompanyAccessTokenManagement {
+
+	@Override
+	public void close() {
+
+	}
+
+	@Override
+	public void setAccessToken(MpAccessTokenStore token) {
+
+	}
+
+	@Override
+	public AccessToken getAccessToken(AccountId accountId) {
+		return null;
+	}
+
+	@Override
+	public AccessToken getAccessToken(CompanyAccountId accountId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAccessToken(CompanyAccessTokenStore token) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
